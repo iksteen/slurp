110a58b57af8eafd8a3e90d5af6fa7e661f6414c
Ingmar Steen <iksteen@gmail.com>
2018-04-29 23:58:31 +0200

    Make extract_archive a function instead of a static method.

--- slurp/plugins/unrar.py
+++ slurp/plugins/unrar.py
@@ -11,6 +11,34 @@ logger = logging.getLogger(__name__)
 DETECT_PART = re.compile(r'\.part(\d+)\.rar')
 
 
+def extract_archive(path):
+    archive_dir, archive_filename = os.path.split(path)
+    extract_dir = os.path.join(
+        archive_dir,
+        '__extracted__',
+        os.path.splitext(archive_filename)[0],
+    )
+    if not os.path.isdir(extract_dir):
+        os.makedirs(extract_dir)
+
+    rf = rarfile.RarFile(path)
+    rf.extractall(extract_dir)
+
+    extracted_files = []
+    for f in rf.infolist():
+        if f.isdir():
+            continue
+
+        native_filename = f.filename.replace('\\', '/')
+
+        extracted_files.append((
+            os.path.join(extract_dir, native_filename),
+            f.file_size
+        ))
+
+    return extracted_files
+
+
 class UnrarProcessingPlugin:
     priority = 10
 
@@ -29,7 +57,7 @@ class UnrarProcessingPlugin:
         async with self.sem:
             async def process_archive(path):
                 try:
-                    return await self.loop.run_in_executor(None, self._extract_archive, path)
+                    return await self.loop.run_in_executor(None, extract_archive, path)
                 except:
                     logger.exception('Failed to extract RAR archive {}:'.format(path))
                     return []
@@ -54,31 +82,3 @@ class UnrarProcessingPlugin:
             except:
                 logger.exception('Failed concatenate file list:')
                 return files
-
-    @staticmethod
-    def _extract_archive(path):
-        archive_dir, archive_filename = os.path.split(path)
-        extract_dir = os.path.join(
-            archive_dir,
-            '__extracted__',
-            os.path.splitext(archive_filename)[0],
-        )
-        if not os.path.isdir(extract_dir):
-            os.makedirs(extract_dir)
-
-        rf = rarfile.RarFile(path)
-        rf.extractall(extract_dir)
-
-        extracted_files = []
-        for f in rf.infolist():
-            if f.isdir():
-                continue
-
-            native_filename = f.filename.replace('\\', '/')
-
-            extracted_files.append((
-                os.path.join(extract_dir, native_filename),
-                f.file_size
-            ))
-
-        return extracted_files
